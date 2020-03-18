27f22db3e40344b20e798f6364f691eccccc3762
Michael Hernandez <gammabeta010@gmail.com>
2020-03-18 21:46:12 +0100

    Finito Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
 $(document).ready(function() {
 
-    //var source = $('#card-template').html();
-    //var template = Handlebars.compile(source);
+    var source = $('#card-template').html();
+    var template = Handlebars.compile(source);
 
     //console.log(template);
+    $('#submit-search').click(function(event){
+        var searchInput = $('#input-search').val();
+        $('#input-search').val('');
+        console.log("User submitted: " + searchInput);
+        $.ajax({
+            url : 'https://api.themoviedb.org/3/search/movie',
+            data: {
+                api_key: 'b1bf34ce942f162a150cd71faf814c6d',
+                query: searchInput//'back to the future'
+            },
+            method : 'GET',
+            success: function(data) {
+                console.log(data.results);
 
-    $.ajax({
-        url : 'https://flynn.boolean.careers/exercises/api/array/music',
-        method : 'GET',
-        success: function(data) {
-            //console.log(data.response);
+                var filmList = data.results;
 
-            var albumList = data.response;
+                for (var i = 0; i < filmList.length; i++) {
 
-            for (var i = 0; i < albumList.length; i++) {
-                //console.log(albumList[i]);
 
-                var album = {
-                    imgAlbum: albumList[i].poster,
-                    albumName: albumList[i].title,
-                    artist: albumList[i].author,
-                    genre: albumList[i].genre,
-                    year: albumList[i].year
+                    var film = {
+                        titolo: filmList[i].title,
+                        titoloOriginale: filmList[i].original_title,
+                        lingua: filmList[i].original_language,
+                        voto: filmList[i].vote_average,
+
+                    }
+
+                    var filmsToShow = template(film);
+                    $('.card-select').append(filmsToShow);
+
                 }
 
-                var cardAlbum = template(album);
-                $('.card-block').append(cardAlbum);
-
+            },
+            error: function() {
+            alert('Failed to call API')
             }
-        },
-        error: function() {
-        alert('Failed to call API')
-        }
 
 
-    });
-
+        });
+    })
 
 
 })
