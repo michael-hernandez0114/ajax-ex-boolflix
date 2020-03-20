d7f5f192e916b41e8fc19ded6095b79a2e8cceb5
Michael Hernandez <gammabeta010@gmail.com>
2020-03-20 12:37:05 +0100

    WIP Milestone 2

--- js/main.js
+++ js/main.js
@@ -5,55 +5,61 @@ $(document).ready(function() {
 
     $('#input-search').keyup(function(event){
         if(event.keyCode == 13) {
-            search();
+            $('#card-container').empty();
+            search('movie');
+            search('tv');
         }
-
     });
 
     //console.log(template);
     $('#submit-search').click(function(event){
-        search();
-    });
-
-    function search() {
         $('#card-container').empty();
+
         var searchInput = $('#input-search').val();
         $('#input-search').val('');
         console.log("User submitted: " + searchInput);
+        search('movie', searchInput);
+        search('tv', searchInput);
+    });
+
+    function search(category, searchFilter) {
+
+        var baseURL = 'https://api.themoviedb.org/3';
+
         $.ajax({
-            url : 'https://api.themoviedb.org/3/search/movie',
+            url : baseURL + '/search/' + category,
             data: {
                 api_key: 'b1bf34ce942f162a150cd71faf814c6d',
-                query: searchInput//'back to the future'
+                query: searchFilter//'back to the future'
             },
             method : 'GET',
             success: function(data) {
-                console.log(data);
-                console.log(data.results);
-
-                var filmList = data.results;
-
-                for (var i = 0; i < filmList.length; i++) {
-
-
-                    var film = {
-                        titolo: filmList[i].title,
-                        titoloOriginale: filmList[i].original_title,
-                        lingua: filmList[i].original_language,
-                        voto: filmList[i].vote_average,
-
-                    }
-
-                    var filmsToShow = template(film);
-                    $('#card-container').append(filmsToShow);
-
-                }
-
+                //console.log(data);
+                //console.log(data.results);
+                var results = data.results;
+                processResults(category, results)
             },
             error: function() {
             alert('Failed to call API')
             }
         });
+    }
+
+    function processResults(category, resultsObj) {
+
+        for (var i = 0; i < resultsObj.length; i++) {
+            var singleObj = {
+                            titolo: resultsObj[i].title,
+                            titoloOriginale: resultsObj[i].original_title,
+                            lingua: resultsObj[i].original_language,
+                            voto: resultsObj[i].vote_average,
+
+                        }
+
+            var objToShow = template(singleObj);
+            $('#card-container').append(objToShow);
+
+        }
     }
 
 
